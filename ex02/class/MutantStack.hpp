b3bee3925845f35656863e940570e2f4186a2451
Eguelin <eguelin@student.42lyon.fr>
2023-12-11 15:35:39 +0100

    finish ex02

--- ex02/class/MutantStack.hpp
+++ ex02/class/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 19:00:46 by eguelin           #+#    #+#             */
-/*   Updated: 2023/12/10 20:03:04 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/12/11 13:50:15 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,64 +45,19 @@ class MutantStack: public std::stack<T>
 		MutantStack	&operator=( const MutantStack &src )
 		{
 			if (this != &src)
-			{
-				this->_begin = src._begin;
-				this->_end = src._end;
-			}
+				this->c = src.c;
 
 			return (*this);
-		}
-
-/* ************************************************************************** */
-/*                               Iterator class                               */
-/* ************************************************************************** */
-
-		class	iterator: public std::iterator<std::bidirectional_iterator_tag, T>
-		{
-			public:
-
-				iterator( void ) {};
-				iterator( const iterator &src ) {static_cast<void>(src);};
-				~iterator( void ) {};
-
-				iterator	&operator=( const iterator &src ) {static_cast<void>(src);};
-
-				iterator	&operator++( void ) {};
-				iterator	&operator--( void ) {};
-				iterator	operator++( int ) {};
-				iterator	operator--( int ) {};
-
-				bool		operator==( const iterator &src ) {static_cast<void>(src);};
-				bool		operator!=( const iterator &src ) {static_cast<void>(src);};
-
-				T			&operator*( void ) {};
-				T			*operator->( void ) {};
-
-			private:
-
-				T			*_ptr;
-				iterator	*_next;
 		};
 
 /* ************************************************************************** */
 /*                          Public members functions                          */
 /* ************************************************************************** */
 
-		void	push( const T &value ) {static_cast<void>(value);};
-		void	pop( void ) {};
-		T		top( void ) const {};
+		typedef typename std::stack<T>::container_type::iterator	iterator;
 
-		MutantStack::iterator	&begin( void ) const {return (this->_begin);};
-		MutantStack::iterator	&end( void ) const {return (this->_end);};
-
-	private:
-
-/* ************************************************************************** */
-/*                             Private attributes                             */
-/* ************************************************************************** */
-
-		MutantStack::iterator	_begin;
-		MutantStack::iterator	_end;
+		iterator	begin( void ) {return (this->c.begin());};
+		iterator	end( void ) {return (this->c.end());};
 };
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,13 +6,72 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:48:04 by eguelin           #+#    #+#             */
-/*   Updated: 2023/12/10 19:50:46 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/12/11 14:04:34 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <vector>
 
 int main( void )
 {
+	{
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+
+		std::cout << "back: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "size: " << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+
+		std::stack<int> s(mstack);
+	}
+	std::cout << std::endl;
+	{
+		std::vector<int> mstack;
+
+		mstack.push_back(5);
+		mstack.push_back(17);
+
+		std::cout << "back: " << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << "size: " << mstack.size() << std::endl;
+
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
+
+		std::vector<int>::iterator it = mstack.begin();
+		std::vector<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+
 	return (0);
 }
